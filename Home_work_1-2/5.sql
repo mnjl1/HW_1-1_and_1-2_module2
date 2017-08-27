8e5f20b8602a4cf47fa87659ff76777f7ebfed71
mnjl1 <d.manzhula@gmail.com>
2017-08-27 22:51:15 +0300

    initDB.sql ver2.0 added 5.sql

--- Home_work_1-2/5.sql
+++ Home_work_1-2/5.sql
@@ -1,5 +1,4 @@
-SELECT c.customerName, min(Profit) FROM (SELECT customerName, projectName, cost, sum(cost-developers.salary) AS Profit
+SELECT c.customerName, min(Profit) FROM (SELECT customerName, projectName, cost, salary, sum(cost-developers.salary) AS Profit
                                          FROM customers INNER JOIN projects ON customers.customer_id = projects.customer_id
                                            INNER JOIN developers ON projects.developer_id = developers.developer_id
-                                         GROUP BY customerName) as c
-
+                                         GROUP BY projectName) as c
